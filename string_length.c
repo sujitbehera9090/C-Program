5efcd4eebec5e685c0f1b529b0b945f7854596fc
sujitbehera9090 <sujitbehera9979@gmail.com>
2021-08-01 15:36:19 +0530

    C Program

---
+++ string_length.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+
+#include<conio.h>
+
+#include<stdlib.h>
+
+#include<string.h>
+
+int main(){
+
+    char str1[] = "Sujit Kumar Behera";
+
+    int len = strlen(str1);
+
+    system("cls");
+
+    printf("\n\tLength of str1[]: %d\n" , len);
+
+    getch();
+
+    return 0;
+
+}
+
+// string_length.c
