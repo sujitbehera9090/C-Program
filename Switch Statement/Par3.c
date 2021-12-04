44248d79317e33d57b9e439334a71e3bca1f326b
sujitbehera9090 <sujitbehera9979@gmail.com>
2021-12-04 19:38:39 +0530

    Consonant or not using switch

---
+++ Switch Statement/Par3.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+
+int main(){
+
+    char ch;
+
+    printf("Enter a alphabet: ");
+
+    scanf("%c" , &ch);
+
+    switch( ch ){
+
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+        printf("Vowel.");
+        break;
+        
+        default:
+        printf("Consonant.");
+
+    }
+}
